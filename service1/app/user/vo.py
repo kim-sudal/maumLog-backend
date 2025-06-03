9b72aca0a8cecd94a69a79343e104ce99909abaa
khj <khj0326@timesoft.co.kr>
2025-06-04 00:57:50 +0900

    Refactor   : 응답조건 변경경

--- service1/app/user/vo.py
+++ service1/app/user/vo.py
@@ -6,13 +6,12 @@ class UserVO(BaseModel):
     # 요청 시 필드 (회원가입, 로그인, 중복체크 등에 사용)
     login_id: Optional[str] = None
     login_password: Optional[str] = None
-    password_confirm: Optional[str] = None  # 비밀번호 확인용 (회원가입 시)
     user_name: Optional[str] = None
     birth_date: Optional[str] = None  # YYYYMMDD 형식
     nickname: Optional[str] = None  # 
-    phone_number: Optional[str] = None
     email: Optional[str] = None
-    user_description: Optional[str] = None
+    user_description: Optional[str] = None 
+    mbti : Optional[str] = None
     
     # 응답 시 공통 필드
     success: Optional[bool] = None
